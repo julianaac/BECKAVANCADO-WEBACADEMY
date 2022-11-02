9bc1ac197227c015b6d63a1b7a5be19a91cdc8a6
Juliana Abreu <juliana.1419981@gmail.com>
2022-11-01 20:25:51 -0500

    Aula 1

---
+++ first-week/js/index.js
@@ -0,0 +1,3 @@
+console.log('texto')//tanto faz aspas duplas e simples deve seguir o padrão a crase é em literais
+console.error('Essa é uma mensagem de erro ...') 
+console.warn('Isso é um alerta')
