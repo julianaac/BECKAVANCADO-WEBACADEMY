dd1b211727cb17681f3a193b10fff42b512f8e84
Juliana Abreu <juliana.1419981@gmail.com>
2022-11-14 20:43:31 -0500

    aula 14/11

--- project-logger/stats.js
+++ project-logger/stats.js
@@ -7,6 +7,7 @@ const {freemem, totalmem} = os
 
 // console.log(`${parseInt((freemem()/1024/1024))} ${parseInt((totalmem()/1024/1024))}`)
 setInterval(()=>{
+    //decisões de projeto é bom por exxemplo
     const total = parseInt(totalmem()/1024/1024)
     const freeMem = parseInt((freemem()/1024/1024))
     const usage = total - freeMem
@@ -21,6 +22,8 @@ setInterval(()=>{
     //boa prática comentar o que foi criada inserir aqui o que é log porque recebe dois parametros
     console.table(stats)//transformar para texto no caso um json no documento não aguenta
     log('Rodando', JSON.stringify(stats)) //carrega a/as função/oes que foram exportadas nessse módulo
+    //o método log recebe como parametro uma mensagem de confirmação e uma string no caso json contendo a mé moria atual 
+    //no log é gerado o log em outro arquivo contendo a data o horario e a memória utilizada passada
 }, 1000)
 
 
